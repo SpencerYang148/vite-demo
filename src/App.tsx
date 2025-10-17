de59375fedc22a02bdef83d93fafbbd78d885fad
yangshibin <yangshibin@cvte.com>
2025-10-17 17:26:58 +0800

    支持路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
-
+  
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
   )
 }
 
